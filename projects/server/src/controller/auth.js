6013eea9ed231b04e698bbe7c21382cfbfe7d410
damarsasiwilogo <damarsasiwilogo@gmail.com>
2023-11-30 18:44:05 +0700

    temp push

--- projects/server/src/controller/auth.js
+++ projects/server/src/controller/auth.js
@@ -97,7 +97,7 @@ exports.tenantRegister = async (req, res) => {
     email,
     password,
     phoneNumber,
-    // gender,
+    gender,
     dateofbirth,
     // ktpImg,
   } = req.body;
@@ -125,7 +125,7 @@ exports.tenantRegister = async (req, res) => {
       email,
       password: hashPassword,
       phoneNumber,
-      // gender,
+      gender,
       dateofbirth,
       // ktpImg,
       role: "tenant",
@@ -134,7 +134,7 @@ exports.tenantRegister = async (req, res) => {
     // verify email by sending to email
     const token = crypto.randomBytes(20).toString("hex");
     const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
-    const verifyTokenExpiry = Date.now() + 60 * 60 * 1000;
+    const verifyTokenExpiry = addHours(new Date(), 1);
 
     result.verifyToken = tokenHash;
     result.verifyTokenExpiry = verifyTokenExpiry;
